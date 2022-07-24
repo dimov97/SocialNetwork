9c35de3df9e32a60a38b2888fc6289cb01600451
dimov97 <dimov.igor97@gmail.com>
2022-07-24 20:05:19 +0300

    Create Header and Technologies components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+      <Header />
+          <Technologies />
+      </div>
   );
 }
+
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">HOME</a>
+            <a href="#">NEWS</a>
+            <a href="#">MESSAGES</a>
+
+        </div>
+    );
+}
+
+export default Header;
